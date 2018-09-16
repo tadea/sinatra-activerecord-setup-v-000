2379da8212d5513dc2c1853b91fab2e642a7d9da
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-09-16 20:22:38 +0000

    automatically backed up by learn

--- db/migrate/20180916201253_create_dogs.rb
+++ db/migrate/20180916201253_create_dogs.rb
@@ -1,10 +1,10 @@
 class CreateDogs < ActiveRecord::Migration
   def change
-    create_table :dogs do |t|
+    create_table :dogs :environment do |t|
       t.string :name
       t.string :breed
     end
   end
 
-  
+
 end
